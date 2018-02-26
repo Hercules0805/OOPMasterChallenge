40d43025678b34e09cdf7ca886391f337305980a
Hercules0805 <hercules0805@gmail.com>
2018-02-26 12:30:01 -0300

    Initial commit

---
+++ src/com/herc/HealthyBurger.java
@@ -0,0 +1,10 @@
+package com.herc;
+
+public class HealthyBurger extends Hamburger{
+
+    private String rollType = "Brown rye";
+
+    public HealthyBurger(String rollType, String meat, double priceBurger) {
+        super(rollType, meat, priceBurger);
+    }
+}
